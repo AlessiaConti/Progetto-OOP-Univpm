937f2590a46f5b6094666ab2301bd920260fc5e9
Alessia Conti <s1092543@studenti.univpm.it>
2021-12-16 17:54:48 +0100

    Aggiunta classe Coordinate

--- Progetto-OOP-Univpm/openweather/src/main/java/com/univpm/openweather/model/Città.java
+++ Progetto-OOP-Univpm/openweather/src/main/java/com/univpm/openweather/model/Città.java
@@ -5,18 +5,13 @@ public class Città {
 	//classe che definisce la citta' dalla quale si ottengono le previsioni
 	
 	//attributi
-	private int lon;
-	private int lat;
+	private Coordinate coordinate; 
 	private int iD;
 	private String nome;
 	
 	//getter
-	public int getLon() {
-		return lon;
-	}
-	
-	public int getLat() {
-		return lat;
+	public Coordinate getCoordinate() {
+		return coordinate;
 	}
 	
 	public int getiD() {
@@ -28,11 +23,8 @@ public class Città {
 	}
 	
 	//setter
-	public void setLon(int lon) {
-		this.lon = lon;
-	
-	public void setLat(int lat) {
-		this.lat = lat;
+	public void setCoordinate(Coordinate coordinate) {
+		this.coordinate = coordinate;
 	}
 	
 	public void setiD(int iD) {
