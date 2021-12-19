70e3ff0a75f4b8d73dca33e88c3382974023c996
Alessia Conti <s1092543@studenti.univpm.it>
2021-12-19 19:41:25 +0100

    Aggiunta rotta /Forecast al controller
    Rotta di tipo GET che restituisce le previsioni relative a umidità, temperatura effettiva, temperatura percepita della città inserita da utente tramite coordinate

--- Progetto-OOP-Univpm/openweather/src/main/java/com/univpm/openweather/controller/weatherController.java
+++ Progetto-OOP-Univpm/openweather/src/main/java/com/univpm/openweather/controller/weatherController.java
@@ -1,13 +1,29 @@
 package com.univpm.openweather.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.univpm.openweather.model.Coordinate;
 
 
 @Controller 
 public class weatherController {
 	
-	@RequestMapping(value = "/getForecast")
+	@Autowired
+	Service service;
+	
+	//Rotta di tipo GET che mostra le previsioni relative a umidità, temperatura effettiva, 
+	//temperatura percepita della città inserita da utente tramite coordinate
+	
+	@GetMapping(value = "/Forecast")
+	public ResponseEntity<Object> getForecast(@RequestParam Coordinate coordinate) {
+		return new ResponseEntity<> (service.getForecastfromApi(coordinate).toString(), HttpStatus.OK);
+    }
 	
 
 }
